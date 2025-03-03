b6732aa8af97df0fa2236e5a593c227fe1e79d2c
x <x@x.com>
2025-03-04 01:50:41 +1100

    fix error

--- Cloudinary/CLUploader.m
+++ Cloudinary/CLUploader.m
@@ -316,7 +316,7 @@
 
     [body appendData:[[NSString stringWithFormat:@"--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
     [body appendData:[@"Content-Type: application/octet-stream\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
-    [body appendData:[NSData dataWithContentsOfURL:file]];
+    [body appendData:fileData];
     [body appendData:[@"\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
 
     // Add the final boundary
