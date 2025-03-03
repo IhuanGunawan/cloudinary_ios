d535eda608675841db7faf2ecccb9ddfc7acd39c
x <x@x.com>
2025-03-04 01:48:00 +1100

    actually the its not a path

--- Cloudinary/CLUploader.m
+++ Cloudinary/CLUploader.m
@@ -303,25 +303,21 @@
 //    params:params file:file timeout:[options valueForKey:@"timeout"]];
     [req setValue:@"your_custom_value" forHTTPHeaderField:@"X-Custom-Header"];
 
-    NSURL *fileURL = [NSURL fileURLWithPath:file];  // Example file path
-
     NSString *boundary = @"----WebKitFormBoundary7MA4YWxkTrZu0gW";
     NSMutableData *body = [NSMutableData data];
 
     // Add parameters to the body (if necessary)
-//    for (NSString *key in params) {
-//        [body appendData:[[NSString stringWithFormat:@"--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
-//        [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n\r\n", key] dataUsingEncoding:NSUTF8StringEncoding]];
-//        [body appendData:[[params[key] description] dataUsingEncoding:NSUTF8StringEncoding]];
-//        [body appendData:[@"\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
-//    }
+    for (NSString *key in params) {
+        [body appendData:[[NSString stringWithFormat:@"--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n\r\n", key] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[params[key] description] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[@"\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+    }
 
-//    [body appendData:[[NSString stringWithFormat:@"--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
-//        [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"file\"; filename=\"%@\"\r\n",
-//                           [fileURL lastPathComponent]] dataUsingEncoding:NSUTF8StringEncoding]];
-//        [body appendData:[@"Content-Type: application/octet-stream\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
-//        [body appendData:[NSData dataWithContentsOfURL:fileURL]];
-//        [body appendData:[@"\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+    [body appendData:[[NSString stringWithFormat:@"--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+    [body appendData:[@"Content-Type: application/octet-stream\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+    [body appendData:[NSData dataWithContentsOfURL:file]];
+    [body appendData:[@"\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
 
     // Add the final boundary
     [body appendData:[[NSString stringWithFormat:@"--%@--\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
