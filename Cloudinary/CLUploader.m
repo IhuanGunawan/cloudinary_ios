efd593b70ceed2b6658e901ba0edde2f50b8d48a
x <x@x.com>
2025-03-08 12:10:27 +1100

    debug body

--- Cloudinary/CLUploader.m
+++ Cloudinary/CLUploader.m
@@ -303,7 +303,7 @@
 //    params:params file:file timeout:[options valueForKey:@"timeout"]];
     [req setValue:@"your_custom_value" forHTTPHeaderField:@"X-Custom-Header"];
 
-    NSString *boundary = @"----WebKitFormBoundary7MA4YWxkTrZu0gW";
+    NSString *boundary = @"--871ff282dec35ee80";
     NSMutableData *body = [NSMutableData data];
 
     // Add parameters to the body (if necessary)
@@ -316,14 +316,17 @@
 
     [body appendData:[[NSString stringWithFormat:@"--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
     [body appendData:[@"Content-Type: application/octet-stream\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
-    [body appendData:file];
+//    [body appendData:file];
     [body appendData:[@"\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
 
     // Add the final boundary
     [body appendData:[[NSString stringWithFormat:@"--%@--\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
 
     // Set the HTTP body to the created multipart body
-    req.HTTPBody = body;
+//    req.HTTPBody = body;
+    
+    NSString *string = [[NSString alloc] initWithData:body encoding:NSUTF8StringEncoding];
+    [req setValue:string forHTTPHeaderField:@"the body"];
 
     // create the connection with the request and start loading the data
     if ([[_cloudinary get:@"sync" options:options defaultValue:@NO] boolValue]) {
